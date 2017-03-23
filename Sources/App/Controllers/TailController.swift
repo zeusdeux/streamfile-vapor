3e64a93d21c7cc23a8d2567e3d13daecc128dfb7
Mudit Ameta <zeusdeux@gmail.com>
2017-03-23 03:15:16 +0100

    Respond with the output of a shell command

--- Sources/App/Controllers/TailController.swift
+++ Sources/App/Controllers/TailController.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Vapor
 import HTTP
 
@@ -6,7 +7,21 @@ final class TailController: ResourceRepresentable {
         guard let filename = req.data["filename"]?.string else {
             throw Abort.badRequest
         }
-        return "Filename is \(filename)"
+
+        let task = Process()
+
+        task.launchPath = "/usr/bin/tail"
+        task.arguments = ["-n 10", filename]
+
+        let pipe = Pipe()
+        task.standardOutput = pipe
+
+        task.launch()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+
+        return "Filename is \(output!)"
     }
 
     func makeResource() -> Resource<Int> {
